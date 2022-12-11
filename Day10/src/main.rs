578b8a0e47a3283c951ae2e72dcbd4042a2d0a28
Dan Sparks <lordaniel24@gmail.com>
2022-12-11 13:49:17 -0600

    cleanup

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -3,18 +3,45 @@ use std::{
     str::FromStr,
 };
 
-use anyhow::Ok;
-use anyhow::Result;
+struct CRT {
+    cpu: CPU,
+    video: Video,
+}
+
+impl CRT {
+    fn new() -> Self {
+        Self {
+            cpu: CPU::new(),
+            video: Video::new(),
+        }
+    }
+}
+
+struct Video {
+    videobuffer: Vec<char>,
+}
+
+impl Video {
+    fn new() -> Self {
+        Self {
+            videobuffer: Vec::new(),
+        }
+    }
+}
 
 struct CPU {
     x: isize, // x register
     cycles: isize,
-    signal_sum: isize
+    signal_sum: isize,
 }
 
 impl CPU {
     fn new() -> Self {
-        Self { x: 1, cycles: 0, signal_sum: 0}
+        Self {
+            x: 1,
+            cycles: 0,
+            signal_sum: 0,
+        }
     }
 
     fn incrcycle(&mut self) {
@@ -27,7 +54,6 @@ impl CPU {
             180 => self.signal_sum += self.cycles * self.x,
             220 => self.signal_sum += self.cycles * self.x,
             _ => {}
-
         }
     }
 
@@ -63,17 +89,18 @@ impl CPU {
 }
 
 fn main() {
-    let mut arcadeCPU = CPU::new();
+    let mut Arcade = CRT::new();
+    Arcade.video.videobuffer.resize(40, '.');
     let file = std::fs::read_to_string("given.txt").unwrap();
+
     for line in file.lines() {
         let operation = line.get(0..4);
         if line.len() > 4 {
             let operand = line.get(5..);
-            arcadeCPU.execute(operation, operand);
+            Arcade.cpu.execute(operation, operand);
         } else {
-            arcadeCPU.execute(operation, None::<&str>);
+            Arcade.cpu.execute(operation, None::<&str>);
         }
     }
-    println!("CPU Signal Sum: {}", arcadeCPU.signal_sum);
-    
+    println!("CPU Signal Sum: {}", Arcade.cpu.signal_sum);
 }
